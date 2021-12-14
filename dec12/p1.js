9b1690a0562fe7004dbde2fe9167db8f2b4ee7f3
Maakep <schon.fredrik@gmail.com>
2021-12-14 11:31:18 +0100

    12 - Large example doesnt work but input does. We take those

---
+++ dec12/p1.js
@@ -0,0 +1,68 @@
+const input =
+  process.argv[4] == "t" ? require("./input-test") : require("./input");
+module.exports = () => {
+  const finals = [];
+  for (const startPath of viablePathsFrom("start", [])) {
+    const previousSmallPaths = [];
+    const currentPath = ["start"];
+
+    if (isLower(startPath)) previousSmallPaths.push(startPath);
+
+    traverse(startPath, previousSmallPaths, currentPath, finals);
+  }
+  console.log(finals);
+
+  return finals.length;
+};
+
+function traverse(path, previousSmallPaths, currentPath, finals) {
+  const viableNextPaths = viablePathsFrom(path, previousSmallPaths);
+  console.log(
+    currentPath.join(" > "),
+    ">",
+    path,
+    "Next:",
+    viableNextPaths,
+    "Without:",
+    previousSmallPaths
+  );
+  currentPath.push(path);
+
+  for (const newPath of viableNextPaths) {
+    if (currentPath.at(-1) == "end") {
+      finals.push(currentPath);
+      console.log("## ", currentPath.join(" -> "), "\n");
+      return;
+    }
+
+    const newPrevSmallPath = [...previousSmallPaths];
+    if (isLower(newPath)) newPrevSmallPath.push(newPath);
+
+    traverse(newPath, newPrevSmallPath, [...currentPath], finals);
+  }
+
+  return currentPath;
+}
+
+function viablePathsFrom(pathKeyFrom, previousSmallPaths) {
+  return input
+    .filter((x) => {
+      const val = Object.values(x)[0];
+      const key = Object.keys(x)[0];
+
+      return val == pathKeyFrom || key == pathKeyFrom;
+    })
+    .map((x) => {
+      const val = Object.values(x)[0];
+      const key = Object.keys(x)[0];
+      const target = key == pathKeyFrom ? val : key;
+      return target;
+    })
+    .filter((x) => !previousSmallPaths.includes(x) && x != "start");
+}
+
+function isLower(pathKey) {
+  return (
+    pathKey == pathKey.toLowerCase() && pathKey != "start" && pathKey != "end"
+  );
+}
